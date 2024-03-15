8ba6726a1a6b1332adb442e3601194700cf5fb7d
Mani <159115403+peace2212@users.noreply.github.com>
2024-03-15 11:52:16 +0530

    Update 2.c

--- 2.c
+++ 2.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <string.h>
 
-// Function to read strings
 void read_strings(char *main_str, char *pattern_str, char *replace_str) {
     printf("Enter the main string: ");
     scanf("%s", main_str);
@@ -11,7 +10,7 @@ void read_strings(char *main_str, char *pattern_str, char *replace_str) {
     scanf("%s", replace_str);
 }
 
-// Function to find and replace pattern occurrences
+
 void find_and_replace(char *main_str, char *pattern_str, char *replace_str) {
     int main_len = strlen(main_str);
     int pattern_len = strlen(pattern_str);
@@ -20,13 +19,13 @@ void find_and_replace(char *main_str, char *pattern_str, char *replace_str) {
     for (int i = 0; i <= main_len - pattern_len; i++) {
         int j;
 
-        // Check if pattern matches at current position
+        
         for (j = 0; j < pattern_len; j++) {
             if (main_str[i+j] != pattern_str[j])
                 break;
         }
 
-        // If pattern found, replace it
+        
         if (j == pattern_len) {
             for (j = 0; j < replace_len; j++) {
                 main_str[i+j] = replace_str[j];
@@ -38,13 +37,13 @@ void find_and_replace(char *main_str, char *pattern_str, char *replace_str) {
 int main() {
     char main_str[100], pattern_str[100], replace_str[100];
 
-    // Read strings
+    
     read_strings(main_str, pattern_str, replace_str);
 
-    // Perform pattern matching and replacement
+    
     find_and_replace(main_str, pattern_str, replace_str);
 
-    // Print the modified main string
+    
     printf("Modified string after pattern matching and replacement: %s\n", main_str);
 
     return 0;
